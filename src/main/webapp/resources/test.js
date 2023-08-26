091841cee340bac0b3786aed0f8f0157ca175585
Ömer Faruk Kurt <kurtomerfaruk@gmail.com>
2023-08-26 11:20:09 +0300

    first commit

--- src/main/webapp/resources/test.js
+++ src/main/webapp/resources/test.js
@@ -1,4 +1,5 @@
 function handleSubmit(xhr, status, args, dialog) {
+    console.log("submit");
     var dialogId = dialog.id.replaceAll(":","\\\:");
     if (args.validationFailed) {
         $( "#"+dialogId ).effect( "shake", {times: 3}, 100 );
